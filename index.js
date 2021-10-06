3dad3ca7dd86038a7e44ef2502940643eaa06d02
jman4u <67943741+jman4u@users.noreply.github.com>
2021-10-05 20:09:10 -0400

    search implemented

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const URL = "https://pokeapi.co/api/v2/"
+const URL = "https://pokeapi.co/api/v2/pokemon/"
 
 //use async function
+
+const searchContainer = document.getElementById("search_image")
+const searchValue = document.getElementById("search-bar")
+
+searchValue.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter')
+        getPokemon(searchValue.value)
+})
+
+async function getPokemon(name) {
+    const res = await fetch(URL + name)
+    const pokemon = await res.json()
+
+    const imglink = pokemon.sprites.front_default
+    const img = document.createElement("img")
+    img.src = imglink
+    img.width = 300
+    searchContainer.appendChild(img)
+}
+
