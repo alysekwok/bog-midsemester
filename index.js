c5ea1033db1d546204e7ac76b91e0b56f33909de
jman4u <67943741+jman4u@users.noreply.github.com>
2021-10-07 19:49:07 -0400

    add prev and next buttons

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const URL = "https://pokeapi.co/api/v2/pokemon/"
 
 const searchContainer = document.getElementById("search_image")
 const searchValue = document.getElementById("search-bar")
+const movesButton = document.getElementById("moves")
 
 searchValue.addEventListener('keypress', (e) => {
     if (e.key === 'Enter')
         getPokemon(searchValue.value)
 })
+
+movesButton.addEventListener('click', (e) => {
+    getMoves(searchValue.value)
+})
+
+async function getMoves(name) {
+    const res = await fetch(URL + name)
+    const pokemon = await res.json()
+    const moves = pokemon.moves
+    const text = document.createElement("p")
+}
 
 async function getPokemon(name) {
     const res = await fetch(URL + name)
