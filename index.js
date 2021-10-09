1b74d8c2c0a7d4a2d41d7fc16f946b064885ed42
jman4u <67943741+jman4u@users.noreply.github.com>
2021-10-09 18:25:13 -0400

    prev and next buttons

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/"
+let id
 
 //use async function
 
@@ -25,6 +26,7 @@ async function getMoves(name) {
 async function getPokemon(name) {
     const res = await fetch(URL + name)
     const pokemon = await res.json()
+    id = pokemon.id
 
     const imglink = pokemon.sprites.front_default
     const img = document.createElement("img")
@@ -42,9 +44,36 @@ async function getPokemon(name) {
 const textContainer = document.getElementById("pokeText")
 const infoButton = document.getElementById("info")
 
+const prevButton = document.getElementById("previous")
+const nextButton = document.getElementById("next")
+
 infoButton.addEventListener('click', (e) => {
     getInfo(searchValue.value)
 })
+
+
+prevButton.addEventListener('click', (e) => {
+    getPrevious(id - 1)
+})
+
+nextButton.addEventListener('click', (e) => {
+    getNext(id + 1)
+})
+
+
+
+async function getPrevious(pokeid) {
+    if (pokeid >= 1) {
+        id--
+        getPokemon(pokeid)
+    }
+}
+
+async function getNext(pokeid) {
+    id++
+    getPokemon(pokeid)
+}
+
 
 async function getInfo(name) {
     const res = await fetch(URL + name)
